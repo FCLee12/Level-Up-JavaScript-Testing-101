8c4d036b72c2427cbaa42b836068b377a2b1afbd
Fue Lee <fueclee.12@gmail.com>
2023-06-28 10:00:01 -0500

    wrote first unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,25 @@
-test('Fake Test', () => {
-    expect(true).toBeTruthy();
-});
+import { add } from './App';
+
+// for descriptor/test "name", good convention
+    // is to simply name it the thing you're testing
+
+test('add', () => {
+    const value = add(1, 2);
+    // we expect (assert) variable value to be 3
+    expect(value).toBe(3);
+})
+
+// ***** SAMPLE TEST *****
+
+// test('Fake Test', () => {
+//     expect(true).toBeTruthy();
+// });
 
 // expects are called assertions
     // you are asserting that things will be some way
+    // general format of assertions:
+        // we expect(expect(something)) to be
+            // (.toBe(anotherSomething)) something
+
+// ***** END SAMPLE TEST *****
+
