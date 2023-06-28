5a4b28c3f14ed8192151718a9ce5e5ba769e004f
Fue Lee <fueclee.12@gmail.com>
2023-06-28 10:11:05 -0500

    video #3 completed, Unit Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,5 @@
 import { add } from './App';
 
-// for descriptor/test "name", good convention
-    // is to simply name it the thing you're testing
-
-test('add', () => {
-    const value = add(1, 2);
-    // we expect (assert) variable value to be 3
-    expect(value).toBe(3);
-})
 
 // ***** SAMPLE TEST *****
 
@@ -22,4 +14,33 @@ test('add', () => {
             // (.toBe(anotherSomething)) something
 
 // ***** END SAMPLE TEST *****
+
+// NOTE:
+    // for descriptor/test "name", good convention
+        // is to simply name it the thing you're testing
+
+// ***** UNIT TEST *****
+
+test('add', () => {
+    const value = add(1, 2);
+    // we expect (assert) variable value to be 3
+    expect(value).toBe(3);
+
+    // alt:
+    expect(add(1,2)).toBe(3);
+    expect(add(5,2)).toBe(7);
+})
+
+    // Unit Tests:
+        // only tests one thing
+            // EX: A React component only renders itself, not its children
+        // as opposed to Integration Tests:
+            // EX: functions that runs other functions,
+                // React components that trigger other components to render
+
+// ***** END UNIT TEST *****
+
+
+
+
 
